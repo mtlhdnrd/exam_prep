616d123d50ba762ac15804c95500db69d73ed77c
0pia <bako.borka2005@tanulo.boronkay.hu>
2024-06-03 21:58:42 +0200

    I think it done for now, meg asztali

---
+++ tetelek.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE IF NOT EXISTS tetelek CHARACTER SET utf8 COLLATE utf8_hungarian_ci;
+USE tetelek;
+CREATE TABLE IF NOT EXISTS tantargyak(
+    id int PRIMARY KEY AUTO_INCREMENT,
+    tantargy varchar(225)
+);
+USE tetelek;
+CREATE TABLE IF NOT EXISTS tetelcimek (
+    id int AUTO_INCREMENT,
+    cim varchar(225),
+    vazlat varchar(2550),
+    kidolgozas varchar(4000),
+    modosit DATE,
+    tantargyid int,
+    PRIMARY KEY (id),
+    FOREIGN KEY (tantargyid) REFERENCES tantargyak(id)
+);
+INSERT INTO tantargyak (tantargy) VALUES ("Történelem"),("Irodalom"),("Nyelvtan");
+INSERT INTO tetelcimek (cim, vazlat, kidolgozas, modosit, tantargyid) VALUES ("anyad","faszat","geci",'2000-02-02',2); 
